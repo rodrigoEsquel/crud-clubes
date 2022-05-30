1b9ee4c3a521d235c6b720f6f6f8361da975263c
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-05-29 23:35:17 -0300

    Feat new endpoints to the API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import express from 'express';
-import { engine } from 'express-handlebars';
+import { create } from 'express-handlebars';
+import fs from 'fs';
 
 const app = express();
 const PUERTO = 8080;
+const hbs = create();
 
-app.engine('handlebars', engine());
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.set('views', './src/views/');
 
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('home', {
+    layout: 'main',
+  });
+});
+
+app.get('/team/:id', (req, res) => {
+  try {
+    const equipos = fs.readFileSync('./src/data/equipos.db.json');
+    res.render('team', {
+      layout: 'main',
+      data: equipos.filter((e) => (e.name?.replace(' ', '_') === req.params.id)),
+    });
+  } catch (error) {
+    console.log(error);
+    res.send(`404: "${req.params.id}" not found`);
+  }
 });
 
 app.listen(PUERTO);
