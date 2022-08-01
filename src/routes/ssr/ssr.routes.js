dfc8f026c79a7d614e9bcdcfe40cff7b2840853d
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-07-31 22:05:10 -0300

    Cleans up routes

--- src/routes/ssr/ssr.routes.js
+++ src/routes/ssr/ssr.routes.js
@@ -5,7 +5,7 @@ import {
   teams, getTeamByTla, createTeam, editTeam, deleteTeam, saveImage, loadForm,
 } from '../../database.js';
 import {
-  renderList, renderTeamInView, emptyTeam, handleEditForm, handleNewForm,
+  renderList, renderTeamInView, renderOkTask, emptyTeam, handleEditForm, handleNewForm,
 } from './callback-functions.js';
 
 const router = Router();
@@ -15,74 +15,6 @@ router.get('/ssr/main/:id', renderTeamInView('team_show'));
 router.get('/ssr/main/:id/edit', renderTeamInView('team_edit'));
 router.get('/ssr/main/:id/delete', renderTeamInView('team_delete'));
 router.get('/ssr/new', renderTeamInView('team_edit', emptyTeam));
-router.post('/ssr/main/:id/edit', loadForm, handleEditForm());
-router.post('/ssr/new', loadForm, handleNewForm(), saveImage('uploaded_file'));
-
-/*
-router.post('/ssr/main/:id/edit', loadForm(), (req, res) => {
-  const { pass, response } = validateForm({ ...req.body });
-  try {
-    if (pass) {
-      editTeam({ ...response, originalTla: req.params.id });
-      res.render('resultado_form', {
-        layout: 'main',
-        mensaje: 'Éxito!',
-      });
-    } else {
-      const errorTeam = { ...response, area: { name: response.areaName } };
-      res.render('team_edit', {
-        layout: 'main',
-        team: errorTeam,
-      });
-    }
-  } catch (error) {
-    res.status(500).render('server_error', {
-      layout: 'main',
-    });
-  }
-});
-*/
-
-router.post(
-  '/ssr/new',
-  (req, res, next) => {
-    console.log('req.body', req.body);
-    try {
-      const { pass, response } = validateForm({ ...req.body, task: 'edit' });
-      if (pass) {
-        createTeam(response);
-        next();
-      } else {
-        const errorTeam = { ...response, area: { name: response.areaName } };
-        res.render('team_edit', {
-          layout: 'main',
-          team: errorTeam,
-        });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).render('server_error', {
-        layout: 'main',
-      });
-    }
-  },
-
-  saveImage('uploaded_file'),
-
-  (req, res) => {
-    try {
-      res.render('resultado_form', {
-        layout: 'main',
-        mensaje: 'Éxito!',
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).render('server_error', {
-        layout: 'main',
-      });
-    }
-  },
-);
 router.post('/ssr/main/:id/edit', loadForm, handleEditForm(), renderOkTask());
 router.post('/ssr/new', loadForm, handleNewForm(), saveImage('uploaded_file'), renderOkTask());
 
