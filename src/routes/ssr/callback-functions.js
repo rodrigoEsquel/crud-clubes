29f7f3c12e9df8f6bd59af70fc19761781060884
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-07-31 23:01:54 -0300

    Rearrange export in a single object

--- src/routes/ssr/callback-functions.js
+++ src/routes/ssr/callback-functions.js
@@ -2,109 +2,10 @@ import validateForm from '../../validateForm.js';
 import DB from '../../database.js';
 
 const {
-  getTeams, getTeamByTla, writeTeam, deleteTeam, saveImage, loadForm,
+  getTeams, getTeamByTla, writeTeam, deleteTeam, loadForm, saveImage,
 } = DB;
 
-export function renderList() {
-  return ((_req, res) => {
-    try {
-      res.render('list', {
-        layout: 'main',
-        teams: getTeams(),
-      });
-    } catch (error) {
-      res.status(500).render('server_error', {
-        layout: 'main',
-      });
-    }
-  });
-}
-
-export function renderOkTask() {
-  return ((_req, res) => {
-    try {
-      res.render('resultado_form', {
-        layout: 'main',
-        mensaje: 'Éxito!',
-      });
-    } catch (error) {
-      res.status(500).render('server_error', {
-        layout: 'main',
-      });
-    }
-  });
-}
-
-export function handleEditForm() {
-  return ((req, res, next) => {
-    try {
-      const { pass, response } = validateForm({ ...req.body, task: 'edit' });
-      if (pass) {
-        writeTeam(response);
-        next();
-      } else {
-        res.render('team_edit', {
-          layout: 'main',
-          team: response,
-        });
-      }
-    } catch (error) {
-      console.log(error.mensaje);
-      res.status(500).render('server_error', {
-        layout: 'main',
-      });
-    }
-  });
-}
-
-export function handleNewForm() {
-  return ((req, res, next) => {
-    try {
-      const { pass, response } = validateForm({ ...req.body });
-      if (pass) {
-        writeTeam(response);
-        next();
-      } else {
-        res.render('team_edit', {
-          layout: 'main',
-          team: response,
-        });
-      }
-    } catch (error) {
-      console.log(error.mensaje);
-      res.status(500).render('server_error', {
-        layout: 'main',
-      });
-    }
-  });
-}
-
-export function renderTeamInView(view, team = 'default') {
-  return ((req, res) => {
-    try {
-      const fetchedTeam = (team === 'default'
-        ? getTeamByTla(req.params.id)
-        : team);
-      if (fetchedTeam) {
-        res.render(view, {
-          layout: 'main',
-          team: fetchedTeam,
-        });
-      } else {
-        res.status(404).render('404', {
-          layout: 'main',
-          param: req.params.id,
-        });
-      }
-    } catch (error) {
-      res.status(500).render('server_error', {
-        layout: 'main',
-      });
-    }
-  });
-}
-
-export const emptyTeam = {
+const emptyTeam = {
   name: '',
   tla: '',
   area: { name: '' },
@@ -112,16 +13,124 @@ export const emptyTeam = {
   email: '',
 };
 
-export function removeTeam() {
-  return ((req, res, next) => {
-    try {
-      deleteTeam(req.params.id);
-      next();
-    } catch (error) {
-      console.error(error);
-      res.status(500).render('server_error', {
-        layout: 'main',
-      });
-    }
-  });
-}
+const routesFunctions = {
+
+  loadForm,
+
+  saveImage,
+
+  renderList() {
+    return ((_req, res) => {
+      try {
+        res.render('list', {
+          layout: 'main',
+          teams: getTeams(),
+        });
+      } catch (error) {
+        res.status(500).render('server_error', {
+          layout: 'main',
+        });
+      }
+    });
+  },
+
+  renderTeamInView(view, team = 'default') {
+    return ((req, res) => {
+      try {
+        const fetchedTeam = (team === 'default'
+          ? getTeamByTla(req.params.id)
+          : emptyTeam);
+        if (fetchedTeam) {
+          res.render(view, {
+            layout: 'main',
+            team: fetchedTeam,
+          });
+        } else {
+          res.status(404).render('404', {
+            layout: 'main',
+            param: req.params.id,
+          });
+        }
+      } catch (error) {
+        res.status(500).render('server_error', {
+          layout: 'main',
+        });
+      }
+    });
+  },
+
+  renderOkTask() {
+    return ((_req, res) => {
+      try {
+        res.render('resultado_form', {
+          layout: 'main',
+          mensaje: 'Éxito!',
+        });
+      } catch (error) {
+        res.status(500).render('server_error', {
+          layout: 'main',
+        });
+      }
+    });
+  },
+
+  handleNewForm() {
+    return ((req, res, next) => {
+      try {
+        const { pass, response } = validateForm({ ...req.body });
+        if (pass) {
+          writeTeam(response);
+          next();
+        } else {
+          res.render('team_edit', {
+            layout: 'main',
+            team: response,
+          });
+        }
+      } catch (error) {
+        console.log(error.mensaje);
+        res.status(500).render('server_error', {
+          layout: 'main',
+        });
+      }
+    });
+  },
+
+  handleEditForm() {
+    return ((req, res, next) => {
+      try {
+        const { pass, response } = validateForm({ ...req.body, task: 'edit' });
+        if (pass) {
+          writeTeam(response);
+          next();
+        } else {
+          res.render('team_edit', {
+            layout: 'main',
+            team: response,
+          });
+        }
+      } catch (error) {
+        console.log(error.mensaje);
+        res.status(500).render('server_error', {
+          layout: 'main',
+        });
+      }
+    });
+  },
+
+  handleDeleteTeam() {
+    return ((req, res, next) => {
+      try {
+        deleteTeam(req.params.id);
+        next();
+      } catch (error) {
+        console.error(error);
+        res.status(500).render('server_error', {
+          layout: 'main',
+        });
+      }
+    });
+  },
+};
+
+export default routesFunctions;
