50240d09c58f138c12b6d3e7ee66538de723f606
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-08-11 21:51:28 -0300

    Add areaName test

--- src/validation/__test__/validateData.test.js
+++ src/validation/__test__/validateData.test.js
@@ -5,3 +5,29 @@ import {
   validarAreaName, validarEmail, validarName, validarTla, validarWebsite,
 } from '../validateData.js';
 
+describe('Item validation from inputs', () => {
+  describe('Area Name validations', () => {
+    test('England should pass', () => {
+      const response = validarAreaName('England');
+
+      expect(response.pass).toStrictEqual(true);
+      expect(response.res).toBe('England');
+    });
+    test('Argentina should not pass', () => {
+      const response = validarAreaName('Argentina');
+
+      expect(response.pass).toStrictEqual(false);
+      expect(response.res).toBe('Area name should be England');
+    });
+    test('Empty array should not pass', () => {
+      const response = validarAreaName('');
+      expect(response.pass).toStrictEqual(false);
+      expect(response.res).toBe('Area name should be England');
+    });
+    test('Undefined should not pass', () => {
+      const response = validarAreaName();
+      expect(response.pass).toStrictEqual(false);
+      expect(response.res).toBe('Area name should be England');
+    });
+  });
+});
