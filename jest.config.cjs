45d8075156818d3b4f7906d31ca9fb38cf0fc5b8
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-08-13 00:22:45 -0300

    Install babel to run Jest with modules

--- jest.config.cjs
+++ jest.config.cjs
@@ -24,6 +24,10 @@ module.exports = {
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
+
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
   //   "\\\\node_modules\\\\"
