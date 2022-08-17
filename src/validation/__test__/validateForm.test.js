6e6c4a68e9e8bb0ac99afe9b762d6b0fc1c127be
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-08-17 00:28:11 -0300

    Add ok case

--- src/validation/__test__/validateForm.test.js
+++ src/validation/__test__/validateForm.test.js
@@ -8,4 +8,16 @@ describe('Form validation', () => {
   afterEach(() => {
     jest.restoreAllMocks();
   });
+  describe('Result testing', () => {
+    test('Correct data should pass and get team info', () => {
+      const teamData = {
+        name: 'Test FC', email: 'test@test.com', website: 'www.test.com', areaName: 'England', tla: 'AAA',
+      };
+
+      const response = validateForm(teamData);
+
+      expect(response.pass).toStrictEqual(true);
+      expect(JSON.stringify(response.response)).toEqual('{"name":"Test FC","email":"test@test.com","website":"www.test.com","area":{"name":"England"},"tla":"AAA"}');
+    });
+  });
 });
