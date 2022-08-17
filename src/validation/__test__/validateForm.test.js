87b3a0b70659a8ed610d28026e654761647469fb
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-08-17 00:27:33 -0300

    Add validation form test

---
+++ src/validation/__test__/validateForm.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable arrow-body-style */
+/// <reference types="jest" />
+
+import { jest } from '@jest/globals';
+import validateForm from '../validateForm.js';
+
+describe('Form validation', () => {
+});
