c27dcd6c564e1b923f0fd66e809de392d50fae67
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-08-17 22:27:30 -0300

    Create Team class

---
+++ src/classes/team.js
@@ -0,0 +1,12 @@
+class Team {
+  constructor(team) {
+    this.area.name = team.area;
+    this.name = team.name;
+    this.tla = team.name;
+    this.website = team.name;
+    this.email = team.name;
+    this.id = team.id;
+  }
+}
+
+export default Team;
